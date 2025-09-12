83e02ced3f7ccb53ce062d3ff9cce98847b1b46d
Nik <tabola1000@gmail.com>
2025-09-12 22:09:54 +0300

    Updates

---
+++ _next/static/chunks/d6b95dc9d9c1db3e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/6b297c6bc770653d.js",
+  "static/chunks/d34973633800cf3c.js",
+  "static/chunks/turbopack-940b09f022cf1277.js"
+])
---
+++ _next/static/gZRJouY9_FDbW8DAbNzfV/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/2b043f347e3c93fa.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/conspectus//_next/:path+",
+        "destination": "/conspectus/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
